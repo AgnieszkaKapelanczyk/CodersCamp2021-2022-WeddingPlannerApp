03951977dda05ab8f15f486c58628740a728f687
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2022-04-13 15:03:58 +0200

    footer

--- src/components/App/AppFooter.tsx
+++ src/components/App/AppFooter.tsx
@@ -5,18 +5,18 @@ import gitLogo from '../../assets/img/githubLogo.png'
 
 const AppFooter = () => {
   return (
-    <footer style={{backgroundColor: '#F8EDEB', position:'relative'}}>
+    <footer style={{backgroundColor: '#F8EDEB', position:'static'}}>
       <Box sx={{display:"flex", justifyContent:"space-between" ,alignItems:"flex-end", height:"8rem"}}>
       <Box sx={{display:"flex", justifyContent:"flex-start" , alignItems:"flex-end", margin:'10px'}}>
 
-      <Typography padding='15px' paddingLeft='25px'  variant='body1' paragraph={true} >Projekt został zrealizowany w ramach:</Typography>
+      <Typography  paddingLeft='25px' paddingRight='20px' variant='body1' paragraph={true} >Projekt został zrealizowany w ramach:</Typography>
       <Link href='https://www.coderscamp.edu.pl/'>
-      <img src={codersLogo} height="60px" />
+      <img src={codersLogo} height="35px" />
       </Link>
       </Box>
       <Box sx={{padding:'20px', paddingRight:'30px'}}>
         <Link href='https://github.com/AgnieszkaKapelanczyk/CodersCamp2021-2022-WeddingPlannerApp'>
-      <img src={gitLogo} height="40px" />
+      <img src={gitLogo} height="20px" />
         </Link>
       </Box>
       </Box>
