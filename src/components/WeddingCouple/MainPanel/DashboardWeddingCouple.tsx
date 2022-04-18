2c01078f54c17724639743faf198ee62f26774eb
angbur <41025473+angbur@users.noreply.github.com>
2022-04-18 19:54:32 +0200

    feat: add responsive to widgets

--- src/components/WeddingCouple/MainPanel/DashboardWeddingCouple.tsx
+++ src/components/WeddingCouple/MainPanel/DashboardWeddingCouple.tsx
@@ -1,5 +1,5 @@
 import { Card } from "@material-ui/core";
-import RGL, { WidthProvider } from "react-grid-layout";
+import { Responsive, WidthProvider } from "react-grid-layout";
 import { styled } from '@mui/material/styles';
 import PanelThemeBg from "../../../assets/img/rusticTheme.jpg";
 import TimerWidget from "../../Widgets/TimerWidget";
@@ -18,6 +18,7 @@ const Widget = styled(Card)(({ theme }) => ({
 }));
 
 type ResizeHandle = 's' | 'w' | 'e' | 'n' | 'sw' | 'nw' | 'se' | 'ne';
+
 
 type ItemCallback = (
   layout: Layout[],
@@ -70,17 +71,20 @@ interface LayoutProps {
   onDrop?(layout: Layout[], item: Layout, e: Event): void;
   onDropDragOver?(e: DragOverEvent): { w?: number, h?: number } | false | undefined;
   innerRef?: React.Ref<HTMLDivElement>;
-  cols?: number | undefined;
   layout?: Layout[] | undefined;
   margin?: [number, number] | undefined;
   children?: React.ReactNode;
   containerPadding?: [number, number] | undefined;
   onLayoutChange?(layout: Layout[]): void;
+  cols?: { [P: string]: number } | undefined;
+  breakpoint?: string | undefined;
+  breakpoints?: { [P: string]: number } | undefined;
 }
 
 const Props: LayoutProps = {
+  breakpoints: { "lg": 1200, "md": 996, "sm": 768, "xs": 480, "xxs": 0 },
+  cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
   className: "layout",
-  cols: 12,
   rowHeight: 260,
   maxRows: 30,
   width: 1000,
@@ -88,29 +92,52 @@ const Props: LayoutProps = {
   isResizable: true,
   autoSize: true,
   resizeHandles: ['se'],
-  //resizeHandle: 
   isBounded: true,
   verticalCompact: true,
   onLayoutChange: function() {},
   isDraggable: true,
-  //useCSSTransforms: false,
+  //onLayoutChange={handleLayoutChange}
   
 }
 
 const DashboardWeddingCouple = () => {
   const actualLayout = useAppSelector(selectLayout);
-  const WidgetGridLayout   = WidthProvider(RGL);
+  const ResponsiveGridLayout = WidthProvider(Responsive);
+
+  const  layoutMD =  [
+    { i: "TimerWidget", x: 0, y: 0, w: 6, h: 1, minH: 1, minW: 5 },
+    { i: "ReminderWidget", x: 6, y: 1, w: 6, h: 1 },
+    { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+    { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1 },
+    { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+  ];
 
   const weddingThemeStyle={
     backgroundImage: `url(${PanelThemeBg})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     height: '100%',
-};
+  };
   
+  //const handleLayoutChange = (actualLayout, layouts) => {
+  //  localStorage.setItem("grid-layout", JSON.stringify(layouts));
+  //};
+
+  type ResponsiveLayout = Layouts | undefined;
+
+
+
   return (
     <Box style={weddingThemeStyle}>
-        <WidgetGridLayout layout={actualLayout} {...Props}>
+        <ResponsiveGridLayout layouts={{
+          lg: actualLayout[0].lg, 
+          md: actualLayout[1].md,
+          sm: actualLayout[2].sm,
+          xs: actualLayout[3].xs,
+          xxs: actualLayout[4].xxs
+          }} 
+          {...Props}
+          >
         <Widget key='TimerWidget'>
             <TimerWidget/>
         </Widget>
@@ -126,7 +153,7 @@ const DashboardWeddingCouple = () => {
         <Widget key='PhotosWidget'>
             <PhotoWidget/>
         </Widget> 
-      </WidgetGridLayout>
+      </ResponsiveGridLayout>
     </Box>
   )
 }
--- src/store/layoutSlice.tsx
+++ src/store/layoutSlice.tsx
@@ -2,36 +2,78 @@ import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store/store';
 
 type ResizeHandle = 's' | 'w' | 'e' | 'n' | 'sw' | 'nw' | 'se' | 'ne';
+type Breakpoint = 'lg' | "md" | "sm" | "xs" | "xxs";
+
+
 export interface Layout {
-  i: string;
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-  minW?: number | undefined;
-  maxW?: number | undefined;
-  minH?: number | undefined;
-  maxH?: number | undefined;
-  moved?: boolean | undefined;
-  static?: boolean | undefined;
-  isDraggable?: boolean | undefined;
-  isResizable?: boolean | undefined;
-  resizeHandles?: ResizeHandle[] | undefined;
-  isBounded?: boolean | undefined;
+    i: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    minW?: number | undefined;
+    maxW?: number | undefined;
+    minH?: number | undefined;
+    maxH?: number | undefined;
+    moved?: boolean | undefined;
+    static?: boolean | undefined;
+    isDraggable?: boolean | undefined;
+    isResizable?: boolean | undefined;
+    resizeHandles?: ResizeHandle[] | undefined;
+    isBounded?: boolean | undefined;
 }
+
 export interface Layouts {
   [P: string]: Layout[];
 }
 
-const initialState: Layouts = {
-    layout: [
-      { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1 },
-      { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
-      { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
-      { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1 },
-      { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
-    ]
-};
+const initialState: Layouts[]  = [
+      {
+        lg: [
+          { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
+          { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+          { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+        ]
+      },
+      {
+        md: [
+          { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
+          { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+          { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+        ]
+      },
+      {
+        sm: [
+          { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
+          { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+          { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+        ]
+      },
+      {
+        xs: [
+          { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
+          { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+          { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+        ]
+      },
+      {
+        xxs: [
+          { i: "TimerWidget", x: 0, y: 0, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "ReminderWidget", x: 5, y: 1, w: 5, h: 1 },
+          { i: "NotificationWidget", x: 0, y: 1, w: 5, h: 1.5 },
+          { i: "ProgressWidget", x: 5, y: 4, w: 5, h: 1, minH: 1, minW: 5 },
+          { i: "PhotosWidget", x: 5, y: 4, w: 4, h: 1 }
+        ]
+      }
+];
 
 const layoutSlice = createSlice({
     name: 'layout',
@@ -41,10 +83,10 @@ const layoutSlice = createSlice({
         return state;
       },
       addWidget: (state, action) => {
-        state.layout.push(action.payload)
+        //state.layout.push(action.payload)
       },
       removeWidget: (state, action) => {
-        state.layout.splice(action.payload, 1)
+        //state.layout.splice(action.payload, 1)
       }
     },
   });
@@ -53,6 +95,6 @@ const layoutSlice = createSlice({
   
   export default layoutSlice.reducer;
 
-  export const selectLayout = ((state: RootState)=>state.layout.layout);
+  export const selectLayout = ((state: RootState)=>state.layout);
 
   
