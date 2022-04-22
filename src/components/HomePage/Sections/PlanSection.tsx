4eb19adc896763c6cb7a145ac9507b72e21efac5
Marcin Barszcz <marcin.barszcz@op.pl>
2022-04-22 20:45:27 +0200

    feat: new Home Page sections

--- src/components/HomePage/Sections/PlanSection.tsx
+++ src/components/HomePage/Sections/PlanSection.tsx
@@ -1,96 +1,136 @@
-import {Card, CardHeader, CardContent, Box, Divider, Typography, Avatar, Button} from '@mui/material';
+import { Card, CardHeader, CardContent, Box, Divider, Typography } from '@mui/material';
 import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
-import { theme} from '../../../theme/theme'
-import logoprzed from '../../../assets/img/logo_przed_zalogowaniem.png'
+import { theme } from '../../../theme/theme';
+import logoprzed from '../../../assets/img/logo_przed_zalogowaniem.png';
 
 const PlanSection = () => {
-
-  const styles={
-    card :{
-      maxWidth:' 300px',
-      minHeight:'400px',
-      margin: '20px'   
+  const styles = {
+    card: {
+      maxWidth: ' 300px',
+      minHeight: '400px',
+      margin: '20px',
     },
-    text:{
+    text: {
       color: theme.palette.primary.main,
       variant: theme.typography.body2,
-      fontSize:"1.1rem"
+      fontSize: '1.1rem',
     },
-    text2:{
+    text2: {
       color: theme.palette.primary.main,
-      fontSize:"2rem",
-      fontWeight:'700'
-  
-    }
-   
-  }
+      fontSize: '2rem',
+      fontWeight: '700',
+    },
+    mainText: {
+      color: '#C26D6D',
+      fontFamily: 'Abril Fatface',
+      fontSize: '4.5rem',
+      paddingTop: '4%',
+      paddingBottom: '3%',
+    },
+  };
 
-  return(
-  <Box sx={{display:"flex", flexDirection:"row", justifyContent:"center", gap:"3%", flexWrap:'wrap'}}>
-    <Card style={styles.card}>
-      <Box sx={{display:"flex", justifyContent:"center", padding: '10px'}}>
-    <CardHeader   
-    title="18zł/ miesiąc"
-    subheader="STANDARD" 
-    style={styles.text2}
-  />
+  return (
+    <Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignText: 'center' }}>
+        <Typography style={styles.mainText}>Wybierz swój plan</Typography>
       </Box>
-    <CardContent>
-      <Divider />
-      <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-evenly", padding:'10px' }}>
-        <Typography style={styles.text}>
- <Box> <LibraryAddCheckIcon />  Nielimitowana ilość stworzonych projektów zaproszeń</Box> 
- <Box> <LibraryAddCheckIcon /> 3 Motywy ślubu do wyboru </Box>
- <Box> <LibraryAddCheckIcon /> Nielimitowana lista gości </Box>
- <Box> <LibraryAddCheckIcon /> Promocje u naszych partnerów </Box>
- </Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '3%', flexWrap: 'wrap' }}>
+        <Card style={styles.card}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+            <CardHeader title="18zł/ miesiąc" subheader="STANDARD" style={styles.text2} />
+          </Box>
+          <CardContent>
+            <Divider />
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', padding: '10px' }}>
+              <Typography style={styles.text}>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Nielimitowana ilość stworzonych projektów zaproszeń
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> 3 Motywy ślubu do wyboru{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Nielimitowana lista gości{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Promocje u naszych partnerów{' '}
+                </Box>
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+        <Card style={styles.card}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+            <CardHeader
+              avatar={<img src={logoprzed} alt="Logo WeddingPlanner" height="70px" />}
+              title="39zł/ miesiąc"
+              subheader="PREMIUM"
+              style={styles.text2}
+            />
+          </Box>
+          <Divider />
+          <CardContent>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+              <Typography style={styles.text}>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Nielimitowana ilość stworzonych projektów zaproszeń{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Wiele motywów ślubu do wyboru
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> 500MB miejsca na serwerze na utworzenie galerii zdjęć ze ślubu{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Nielimitowana lista gości{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Promocje u naszych partnerów{' '}
+                </Box>
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
+        <Card style={styles.card}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+            <CardHeader title="0zł/ miesiąc" subheader="DARMOWY" style={styles.text2} />
+          </Box>
+          <Divider />
+          <CardContent>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+              <Typography style={styles.text}>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> 3 Projekty zaproszenia{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> 3 Motywy ślubu do wyboru{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Nielimitowana lista gości{' '}
+                </Box>
+                <Box>
+                  {' '}
+                  <LibraryAddCheckIcon /> Powiadomienia o potwierdzeniu przybycia{' '}
+                </Box>
+              </Typography>
+            </Box>
+          </CardContent>
+        </Card>
       </Box>
-    </CardContent>
-    </Card>
-    <Card style={styles.card}>
-    <Box sx={{display:"flex", justifyContent:"center", padding: '10px'}}>
-    <CardHeader
-    avatar={<img src={logoprzed} height='70px' />}
-
-    title="39zł/ miesiąc"
-    subheader="PREMIUM" 
-    style={styles.text2}
-    />
     </Box>
-     <Divider/>
-    <CardContent>
-    <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-evenly"}}>
-    <Typography style={styles.text}>
- <Box> <LibraryAddCheckIcon /> Nielimitowana ilość stworzonych projektów zaproszeń </Box>
- <Box> <LibraryAddCheckIcon /> Wiele motywów ślubu do wyboru</Box>
- <Box> <LibraryAddCheckIcon /> 500MB miejsca na serwerze na utworzenie galerii zdjęć ze ślubu </Box>
- <Box> <LibraryAddCheckIcon /> Nielimitowana lista gości </Box>
- <Box> <LibraryAddCheckIcon /> Promocje u naszych partnerów </Box>
- </Typography>
-      </Box>
-    </CardContent>
-    </Card>
-    <Card style={styles.card}>
-    <Box sx={{display:"flex", justifyContent:"center", padding: '10px'}}>
-    <CardHeader
-      title="0zł/ miesiąc"
-      subheader="DARMOWY"
-      style={styles.text2} />
-      </Box>
-    <Divider/>
-    <CardContent>
-    <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-evenly"}}>
-    <Typography style={styles.text}>
- <Box> <LibraryAddCheckIcon /> 3 Projekty zaproszenia </Box>
- <Box> <LibraryAddCheckIcon /> 3 Motywy ślubu do wyboru </Box>
- <Box> <LibraryAddCheckIcon /> Nielimitowana lista gości </Box>
- <Box> <LibraryAddCheckIcon /> Powiadomienia o potwierdzeniu przybycia </Box>
- </Typography>
-      </Box>
-    </CardContent>
-    </Card>
-  </Box>
-  )
+  );
 };
 
 export default PlanSection;
--- src/components/HomePage/Sections/SubscribeSection.tsx
+++ src/components/HomePage/Sections/SubscribeSection.tsx
@@ -1,5 +1,41 @@
+import { Box, Typography, Button } from '@mui/material';
+import SubscribeSectionImage from '../../../assets/img/subscryption.jpg';
+import { theme } from 'theme/theme';
+
 const SubscribeSection = () => {
-  return <h1>Test</h1>;
+  const styles = {
+    subscribeSection__image: {
+      backgroundImage: `url(${SubscribeSectionImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      height: '100%',
+      width: '100%',
+      paddingTop: '14vh',
+      paddingLeft: '4vw',
+      paddingBottom: '8vh',
+    },
+  };
+
+  return (
+    <Box style={styles.subscribeSection__image}>
+      <Typography
+        style={{
+          color: `${theme.palette.secondary.main}`,
+          fontFamily: `${theme.typography.fontFamily}`,
+          fontSize: '4.4rem',
+        }}
+      >
+        Dołącz do listy subskrybentów
+      </Typography>
+      <Typography style={{ color: `${theme.palette.secondary.main}` }}>
+        Zostać subskrybentem, jeeli chcesz być informowany o nowych funkcjonalnościach <br />
+        naszej aplikacji lub o promocyjnych ofertach od naszych partnerów!
+      </Typography>
+      <Button size="large" variant="contained" color="primary" style={{ margin: '2rem 0', textTransform: 'uppercase' }}>
+        Zaczynam planować swój ślub
+      </Button>
+    </Box>
+  );
 };
 
 export default SubscribeSection;
